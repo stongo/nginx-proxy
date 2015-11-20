c7ede7dfae470e22f84779cd9bdfe44216787eda
Marcus Stong <stongo@gmail.com>
2015-11-20 10:51:36 -0500

    add ssl stuff

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [ -z $proxy_envs ]; then
 fi
 set_vhost () {
     var=$1
-    if [[ $var != *"_PEM"* || $var != *"_KEY"* ]]; then
+    if [[ $var != *"_SSLPEM"* || $var != *"_SSLKEY"* ]]; then
         arr=(${var//=/ })
         name=$(echo "${arr[0]:6}" | tr '[:upper:]' '[:lower:]' | tr '_' '.')
         proxy=${arr[1]}
@@ -23,13 +23,26 @@ set_vhost () {
         echo "$name"
     fi
 }
-# TODO: write pem and key files
-#write_ssl () {
-
-#}
+write_ssl () {
+    var=$1
+    if [[ $var == *"_SSLPEM"* || $var == *"_SSLKEY"* ]]; then
+        arr=(${var//=/ })
+        name=$(echo "${arr[0]}" | cut -c 6-$(expr ${#arr[0]} - 7 ) | tr '[:upper:]' '[:lower:]' | tr '_' '.')
+    fi
+    if [[ $var == *"_SSLPEM"* ]]; then
+        echo "-- writing ssl pem for $name"
+        pem=${arr[1]}
+        echo "$pem" | tee /etc/nginx/ssl-keys/${name}.pem
+    fi
+    if [[ $var == *"_SSLKEY"* ]]; then
+        echo "-- writing ssl key for $name"
+        key=${arr[1]}
+        echo "$key" | tee /etc/nginx/ssl-keys/${name}.key
+    fi
+}
 for value in $proxy_envs; do
-    name=$(set_vhost $value)
-    echo "name: $name"
+    vhost=$(set_vhost $value)
+    ssl=$(write_ssl $value)
 done
 echo "-- starting nginx"
 exec 2>&1 /usr/sbin/nginx -c /etc/nginx/nginx.conf
